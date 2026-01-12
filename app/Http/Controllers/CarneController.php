390dc5a78568f4841d9111224417b45f4031bb9f
Adauto Oliveira <adautonet@gmail.com>
2026-01-12 13:08:13 -0300

    carnes: gerador 2026 - add registrado no atendimento ajsute erro

--- app/Http/Controllers/CarneController.php
+++ app/Http/Controllers/CarneController.php
@@ -60,8 +60,9 @@ class CarneController extends Controller
 					continue;
 				}
 			}
+			chdir( '../storage/app/private/documentos/carnes/' );
 			dd(getcwd());
-			$html->gerarCarne($dest = $html::OUTPUT_SAVE, $save_path = '/storage/app/private/documentos/carnes/'.date('Y-m-').$pessoa->pessoa.'.pdf');
+			$html->gerarCarne($dest = $html::OUTPUT_SAVE, $save_path = date('Y-m-').$pessoa->pessoa.'.pdf');
 
 			//$html->gerarCarne($dest = $html::OUTPUT_SAVE, $save_path = 'documentos/carnes/'.date('Y-m-d_').$pessoa.'.pdf');
 		}
